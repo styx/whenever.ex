07327a83dfbcc5d3c022849996d67986ba98b70f
Mikhail S. Pobolovets <styx.mp@gmail.com>
2016-02-25 08:21:49 +0300

    Init in progress
    Signed-off-by: Mikhail S. Pobolovets <styx.mp@gmail.com>

--- lib/mix/tasks/whenever.ex
+++ lib/mix/tasks/whenever.ex
@@ -7,7 +7,25 @@ defmodule Mix.Tasks.Whenever do
   DSL to define Cron jobs in easy way.
   """
 
+  @config "config/schedule.exs"
+
   def run(_args) do
+    path =
+      System.cwd()
+      |> Path.join(@config)
+
+    if File.exists?(path) do
+      IO.puts "[skip] `#{path}' already exists"
+      #elsif File.exists?(file.downcase)
+      #warn "[skip] `#{file.downcase}' exists, which could conflict with `#{file}'"
+      #elsif !File.exists?(File.dirname(file))
+      #warn "[skip] directory `#{File.dirname(file)}' does not exist"
+    #else
+      #puts "[add] writing `#{file}'"
+      #File.open(file, "w") { |f| f.write(content) }
+    end
+
+    IO.puts "[done] wheneverized!"
   end
 
 end
